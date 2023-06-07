87403517138cd2a8ce454112168c5f1d2b20dca1
9133sranjitha <sranjitha053@gmail.com>
2023-06-07 10:47:29 +0530

    static login page

--- client side(Front_End)/enabling/src/Components/login.js
+++ client side(Front_End)/enabling/src/Components/login.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './login.css';
+//using state 
 function LoginPage() {
   return (
+    <body>
     <div class="cardPart">
         <div class="leftCard">
               <img className='logOne' src="loginLogo.png" alt="no image" />  
         </div>
         <div class="RightCard">
-        <form action="">
-            <label htmlFor="Name"><b>UserName:</b></label>
-            <input type="text" name="Name" placeholder="Enteryourname" id="Name" /><br />
-            <label htmlFor='password'><b>Password:</b></label>
+        <form>
+            <h1>Login</h1>
+            <input type="text" name="Name" placeholder="Enteryourname" id="Name" />
             <input type="text" name="Password" placeholder="*********" id="password" />
+            <button class="LoginBtn">login</button>
+            <p> not registed? <a href='SignUp.js'>SignUp</a></p>
         </form>
         </div>
     </div>
+    </body>
   );
 
 }
