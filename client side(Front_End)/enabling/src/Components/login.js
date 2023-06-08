71cdba80c37aedcbd14b939ca5e063d99bf36aca
9133sranjitha <sranjitha053@gmail.com>
2023-06-09 02:05:33 +0530

    resolved

--- client side(Front_End)/enabling/src/Components/login.js
+++ client side(Front_End)/enabling/src/Components/login.js
@@ -11,8 +11,8 @@ function LoginPage() {
         <div class="RightCard">
         <form>
             <h1>Login</h1>
-            <input type="text" name="Name" placeholder="Enteryourname" id="Name" />
-            <input type="text" name="Password" placeholder="*********" id="password" />
+            <input type="text" name="Name" placeholder="Enteryourname" id="Name" value={intailValue} />
+            <input type="text" name="Password" placeholder="*********" id="password" value={intailpassword}  />
             <button class="LoginBtn">login</button>
             <p> not registed? <a href='SignUp.js'>SignUp</a></p>
         </form>
